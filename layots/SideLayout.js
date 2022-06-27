4e91cbc83608c8a759eb5b1278c2bd0910532ef3
Roseskk <oleg419583@yandex.ru>
2022-06-27 17:07:04 +0300

    explore page

---
+++ layots/SideLayout.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+
+export default function SideLayout({children}) {
+    return(
+        <div className={'max-w-1600 mx-auto flex flex-wrap relative'}>
+            <aside className={'bg-aside w-56 h-screen'}>
+                ASIDE
+            </aside>
+            <div className={'absolute w-screen h-screen bg-black -z-10'}>
+                <Image
+                    className={'opacity-60 '}
+                    src={'/img/bg.png'}
+                    layout={'fill'}
+                />
+            </div>
+            {children}
+        </div>
+    )
+}
---
+++ pages/explore/index.js
@@ -0,0 +1,12 @@
+import SideLayout from "../../layots/SideLayout";
+
+
+export default function Explore() {
+   return(
+       <SideLayout>
+           <div>
+               asdasd12312
+           </div>
+       </SideLayout>
+   )
+}
