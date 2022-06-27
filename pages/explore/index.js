b746724c927413878725bdb51558807c2a5af147
Roseskk <oleg419583@yandex.ru>
2022-06-27 18:20:17 +0300

    explore page

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -5,11 +5,131 @@ import SideLayout from "../../layots/SideLayout";
 export default function Explore() {
    return(
        <SideLayout>
-           <div className={'flex flex-wrap mt-5 ml-5 '}>
-               <ul className={'flex gap-10 flex-wrap '}>
+           <div className={'w-5/6 flex flex-wrap h-screen  p-10  ml-5 overflow-y-scroll  '}>
+               <ul className={'flex gap-10 flex-wrap mb-5  '}>
                    <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
                        <div className={'h-32 object-cover overflow-hidden '}>
                             <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
+                       </div>
+                       <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
+                           <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
+                       </div>
+                   </li>
+                   <li className={'w-56 h-56  bg-white  rounded overflow-hidden cursor-pointer'}>
+                       <div className={'h-32 object-cover overflow-hidden '}>
+                           <Image src={'/img/bg.png'} width={'100%'} height={'100%'} layout={"responsive"} className={'rounded'}/>
                        </div>
                        <div className={'font-montserratRegular p-1 mt-2 text-sm break-words'}>
                            <span className={'break-words'}>Everyone can be an illustrator! Just believe in yourself</span>
