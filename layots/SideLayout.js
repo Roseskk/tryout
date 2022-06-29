15f7132156dfdd4902448b29dd85acc376d6a4fa
Roseskk <oleg419583@yandex.ru>
2022-06-29 13:33:28 +0300

    aside UI

--- layots/SideLayout.js
+++ layots/SideLayout.js
@@ -7,8 +7,18 @@ export default function SideLayout({children}) {
             <aside className={'bg-aside w-1/6 h-screen relative'}>
                 <h1 className={'font-montserratBold text-center text-white text-h'}>TRY OUT!</h1>
                 <span className={'flex items-center justify-center text-white text-xl tracking-widest font-montserratThin'}>Ivan Ivanov</span>
-                <div className={'flex flex-col items-center justify-center mt-5'}>
-                    <input className={'text-lg  rounded p-2 bg-transparent placeholder:text-white border-b-white border-b tracking-widest placeholder-white text-white focus:outline-none focus:ring-0  placeholder:opacity-50'} placeholder={'Pink UI...'} />
+                {/*<div className={'flex flex-col items-center justify-center mt-5'}>*/}
+                {/*    <input className={'text-lg  rounded p-2 bg-transparent placeholder:text-white border-b-white border-b tracking-widest placeholder-white text-white focus:outline-none focus:ring-0  placeholder:opacity-50'} placeholder={'Pink UI...'} />*/}
+                {/*</div>*/}
+                <div className={'flex flex-col gap-4 mt-4'}>
+                    <button className={'flex items-center pl-7 gap-5'}>
+                        <Image src={'/ico/explore.svg'}  width={'20px'} height={'20px'} />
+                        <span className={'text-white text-lg font-montserratRegular'}>Проекты</span>
+                    </button>
+                    <button className={'flex pl-7 gap-5'}>
+                        <Image src={'/ico/search.svg'}  width={'20px'} height={'20px'}  className={''}/>
+                        <span className={'text-white text-lg font-montserratRegular'}>Поиск</span>
+                    </button>
                 </div>
                 <div className={'absolute bottom-3 flex items-center justify-center w-full'}>
                     <button className={'font-montserratThin text-white flex items-center justify-center'}>
