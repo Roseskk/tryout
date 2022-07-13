f103c21377ccdba2092fae3124d7a0c41f733a17
Roseskk <oleg419583@yandex.ru>
2022-07-13 12:06:06 +0300

    card page

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,14 @@
-import {AnimatePresence, motion} from "framer-motion"
-
 import '../styles/globals.css'
 import {Provider} from "react-redux";
 import {store} from "../redux/store";
+import {AnimateSharedLayout} from "framer-motion";
 
 function MyApp({ Component, pageProps, router }) {
   return(
       <Provider store={store}>
-              <motion.div
-                  key={router.asPath}
-                  initial={{ scale:1.1 }}
-                  animate={{ scale: 1 }}
-                  exit={{ scale: 1.1 }}
-                  transition={{ ease: [0.1, 0.2, 0.2, 0.1] }}
-              >
-                  <Component {...pageProps}   />
-              </motion.div>
+          <AnimateSharedLayout>
+              <Component {...pageProps}   />
+          </AnimateSharedLayout>
       </Provider>
   )
 }
