aee3a1f939206b06a019805e6d3d797b22b47d3b
Roseskk <oleg419583@yandex.ru>
2022-07-30 13:12:42 +0300

    sidebar

--- components/aside/user/NavUser.js
+++ components/aside/user/NavUser.js
@@ -9,13 +9,13 @@ export default function NavUser() {
 
 
     const handleListMenu = () => {
-        dispatch(navReducer({isNavList: 'opacity-100 xl:animate-sideList'}))
+        dispatch(navReducer({isNavList: 'opacity-100 md:animate-sideList'}))
     }
 
 
     return(
         <div className={'md:absolute bottom-0 flex items-center justify-center md:w-full'}>
-            <div className={'font-montserratThin text-white  w-full md:px-2 md:py-4 rounded flex items-center  '}>
+            <div className={'font-montserratThin text-white  md:w-full md:px-2 md:py-4 rounded flex items-center  '}>
                 <button onClick={handleListMenu} className={'flex gap-2 items-center w-full '}>
                     <div className={'hidden tablet:block'}>
                         <Image src={'/ico/person.svg'} width={15} height={15} />
--- components/aside/user/user_options/NavUserList.js
+++ components/aside/user/user_options/NavUserList.js
@@ -7,12 +7,14 @@ export default function NavUserList() {
     const isNavListButton = useSelector((state)=> state.nav.navListExitButton);
 
     return(
-        <ul className={`md:relative text-[10px]  absolute  flex items-center right-0 -bottom-5 md:right-1 md:bottom-8  opacity-100 cursor-pointer   flex-col  ${navList} transition ease-in-out delay-300 rounded p-[2px] `}>
-            <li className={`overflow-hidden opacity-20 hover:opacity-100 bg-white pl-2 pr-2  rounded transition ease-in-out delay-300`}>
-                <span className={'text-black font-montserratBold text-[10px]'}>Log Out!</span>
+        <ul className={`md:relative text-[10px] w-full h-[500px] md:h-fit  bg-purple-500 md:bg-transparent z-90  fixed  flex  items-center  right-0 -bottom-5 md:right-1 md:bottom-8  opacity-100 cursor-pointer   flex-col  ${navList} transition ease-in-out delay-300 rounded p-[2px] `}>
+            <li className={`md:block   w-full md:w-fit overflow-hidden opacity-20 hover:opacity-100 bg-white pl-2 pr-2 rounded transition ease-in-out delay-300`}>
+                <span className={'text-black font-montserratBold md:text-[10px] text-[70px] flex items-center justify-center'}>Log Out!</span>
             </li>
-            <li className={`absolute text-black font-montserratBold text-2xl -left-[15px] -top-[5px] ${isNavListButton} hover:rotate-180 hover:text-red-500 hover:scale-150 transition ease-in-out `}>
-                <button onClick={()=> dispatch(navReducer({isNavList: 'opacity-100 xl:animate-sideList hidden',isNavListButton: 'opacity-100'}))}>&times;</button>
+            <li className={`md:absolute md:mt-0 mt-10 flex  items-center justify-center md:block md:text-2xl text-[90px] w-full text-black font-montserratBold text-2xl -left-[15px] -top-[5px] ${isNavListButton}  hover:scale-150 transition ease-in-out `}>
+                <button className={'text-white'} onClick={()=> dispatch(navReducer({isNavList: 'opacity-100 xl:animate-sideList hidden',isNavListButton: 'opacity-100'}))}>
+                    &times;
+                </button>
             </li>
         </ul>
     )
